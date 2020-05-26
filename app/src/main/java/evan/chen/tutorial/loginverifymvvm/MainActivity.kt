f5adc4678e8d5e1aeee151e94aacd8c79f52fcfe
Evan <>
2020-05-26 15:29:39 +0800

    init

---
+++ app/src/main/java/evan/chen/tutorial/loginverifymvvm/MainActivity.kt
@@ -0,0 +1,12 @@
+package evan.chen.tutorial.loginverifymvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
